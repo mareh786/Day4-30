8ef4824acc15b408ad71eccc0af8d009ba27aa05
mareh786 <116783167+mareh786@users.noreply.github.com>
2025-05-04 16:48:34 +0530

    Fixing 5

--- test_app.py
+++ test_app.py
@@ -3,27 +3,23 @@ from app import app
 
 class FlaskAppTestCase(unittest.TestCase):
     def setUp(self):
+        app.testing = True
         self.client = app.test_client()
 
-    def test_home_page(self):
+    def test_home(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Welcome to the Web App!", response.data)
+        self.assertIn(b"Welcome", response.data)
 
     def test_greet(self):
-        name = "Alice"
-        response = self.client.get(f'/greet/{name}')
+        response = self.client.get('/greet/Adil')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Hello, Adil!", response.data)
 
     def test_greet_edge_case(self):
-        name = "123"
-        response = self.client.get(f'/greet/?name={name}')
+        response = self.client.get('/greet/123')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Hello, 123!", response.data)
 
-
-
 if __name__ == '__main__':
     unittest.main()
-
